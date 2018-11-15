57134f4d0332a8ea763874917ce02f8add6e38b5
Aleksandr Scherbachenko <ascherbachenko@cs-cart.com>
2018-11-15 08:18:05 +0400

    Obsolete test removed. Linter notice fixed

--- src/FileParserFactory.php
+++ src/FileParserFactory.php
@@ -31,12 +31,3 @@ function getJsonFileParser(): \Closure
         return json_decode($json, true);
     };
 }
-
-function parseNestedJson($json)
-{
-    $parsed = json_decode($json, true);
-    $result = array_map($parsed, function () {
-        
-    });
-    return $result;
-}
